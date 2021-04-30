d7d8ea8a2af3652076cee9c1215c14ff28ec4b83
Joel <ricci.lob@gmail.com>
2021-04-29 21:16:35 -0700

    Docstring added

--- helper_modules/MDS.py
+++ helper_modules/MDS.py
@@ -2,45 +2,97 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mpltcols
 import matplotlib.patches as mpatches
+from typing import Tuple
 
-def cMDS(D, is_similarity = False):
-    # Modificado de: http://www.nervouscomputer.com/hfs/cmdscale-in-python/
+def cMDS(D: np.ndarray, 
+         is_similarity: bool = False
+         ) -> Tuple:
+
+    '''
+    Computes Classical Multidimensional Scaling from a given distance, or 
+    similarity, matrix D.
+
+    Parameters
+    ----------
+    D : np.ndarray
+        A distance, or similarity, matrix (squared matrix)
+    is_similarity: bool
+        Determines if D is a similarity matrix (True) or a
+        distance matrix (False)
+
+    Returns
+    -------
+    Tuple
+        F.T: np.ndarray
+            transposed configuration matrix F
+        D_sq: np.ndarray
+            squared distance matrix D
+        B: np.ndarray
+            double centering matrix = -0.5*J*D^2*J  
+        e_vals: np.array
+            eigenvalues of B
+
+    Modified from: http://www.nervouscomputer.com/hfs/cmdscale-in-python/
+    '''
+
+    assert D.shape[0] == D.shape[1]
+
     if is_similarity:
-        D = 1 - D # Si es matriz de similitud, se vuelva a distancia
-    # Número de observaciones                                                                
+        D = 1 - D # If D is a similarity matrix, convert it to a distance matrix
+    # Number of samples                                                             
     n = len(D)
-    # Se genera la matriz de distancia al cuadrado (Squared proximity matrix)
+    # Create the Squared proximity matrix
     D_sq = D**2
-    # Se genera la matriz de  centrado J (Centering matrix)
-    # Definida como la matriz de identidad In menos la matriz de nxn unos/n
+    # Generate the Centering matrix: J
+    # Defined as the identity matrix I_n minums an nxn all-ones matrix
     e1 = np.ones((n,1))
     m = ( np.ones((n, 1)) / n)
-    I = np.eye(n) # Matriz de identidad I_n
-    J = I - e1.dot(np.transpose(m)) # Matriz de centrado
-    # Se genera la matriz B, definida como B = -0.5*J*D^2*J                                                                                 
-    B = -(1/2) * J.dot(D_sq).dot(J) # como J es simétrica da lo mismo teansponerla o no
+    I = np.eye(n) # Identity matrix I_n
+    J = I - e1.dot(np.transpose(m)) # Centering matrix J
+    # Generate B = -0.5*J*D^2*J                                                                                 
+    B = -(1/2) * J.dot(D_sq).dot(J)
 
-    # Se determinan los eigenvalores y eigenvectores de la matriz B                                                                         
+    # Compute the eigendecomposition of B                                                                         
     e_vals, e_vecs = np.linalg.eigh(B)
 
-    # Se ordenan los eigenvalores de mayor a menor
-    idx   = np.argsort(e_vals)[::-1] # Se extraen los índices de ordenamiento
-    e_vals = e_vals[idx] # Se ordena cada vector según el eigenvalor
+    # Sort eigenvalues (descending)
+    idx   = np.argsort(e_vals)[::-1] # Get the sorting indices
+    e_vals = e_vals[idx] # Sort eigenvectors
     e_vecs = e_vecs[:,idx]
-    w, = np.where(e_vals > 0) # Se seleccionan los índices donde los eValores son positivos
+    w, = np.where(e_vals > 0) # To keep only positive eigenvalues
     e_vals = e_vals[w]
-    # Se obtiene la matriz de configuración F = M^(-1/2)*E_m*L_m^(1/2)  
-    M_neg_sq = np.diag(m[:,0]**(-0.5)) # M_neg_sq es la matriz M^(-1/2)
-    L_m_sq  = np.diag(np.sqrt(e_vals[w])) #  Lambda_sq (L_m^(1/2)) es la matriz diagonal con la raiz cuadrada de m eigenValores positivos
-    E_m = e_vecs[:,w] # E_m  es la matriz con los m eigenvectores positivos y ordenados
+    # Configuration matrix F = M^(-1/2)*E_m*L_m^(1/2)  
+    M_neg_sq = np.diag(m[:,0]**(-0.5)) # M_neg_sq is the  M^(-1/2) matrix
+    L_m_sq  = np.diag(np.sqrt(e_vals[w])) #  Lambda_sq (L_m^(1/2)) ia a diagonal matrix with the m positive eigenvalues
+    E_m = e_vecs[:,w] # E_m  = sorted eigenvectors (positives only)
     # F  = M_neg_sq.dot(E_m.dot((L_m_sq)))  # F = M^(-1/2)*E_m*L_m^(1/2)
     F = E_m.dot((L_m_sq))
-    #print(B == (X.transpose()).dot(X) )
-
+    # Return the computed matrices
     return F.T, D_sq, B, e_vals
 
-def cMDS_proj(cMDS_obj, sup_point):
-    # Recibe un objeto cMDS_obj
+def cMDS_proj(cMDS_obj: Tuple, 
+              sup_point: np.ndarray) -> np.ndarray:
+    '''
+    Computes Classical Multidimensional Scaling projection given
+    a cMDS_obj (reference space) and a matrix of distances between
+    supplementary and reference points.
+
+    Parameters
+    ----------
+    cMDS_ob : Tuple
+        A tuple with `F.T, D_sq, B, e_vals` values, returned from the
+        cMDS function
+    sup_point: np.ndarray
+        A distance matrix of size nxm, where n is the number of 
+        reference samples and m is the number of supplementary samples 
+
+    Returns
+    -------
+    F_proj: np.ndarray
+        A matrix with supplementary samples projected over the reference space
+
+    Modified from: http://www.nervouscomputer.com/hfs/cmdscale-in-python/
+    '''
     F = cMDS_obj[0].T # Matriz F de scores obtenida con cMDS a partir de los valores "Activos"
     D_sq = cMDS_obj[1] # Matrix Delta^2
     e_vals = cMDS_obj[3] # Eigenvalores positivos obtenidos del B de los valores "Activos"
