11f70a8a3be96c439bbdfefe42cde619f027ed56
Joel <ricci.lob@gmail.com>
2021-09-03 19:55:13 -0700

    Docstrings enriched

--- R_scripts/plot_Critical_Difference.R
+++ R_scripts/plot_Critical_Difference.R
@@ -1,78 +1,122 @@
 library(scmamp)
 library(scales)
 
-full_names <- c('LR', 'GBT', 'DClf', 'csAVG', 'csGEO', 'csMIN')
-cbbPalette <- c( '#785EF0', '#3F93D2', '#44AA99', '#FE6100', '#DC267F', '#FFB000')
+# SBVS methods
+full_names <- c('LR', 'GBT', 'DClf', 
+                'csAVG', 'csGEO', 'csMIN')
+# Color palette
+cbbPalette <- c( '#785EF0', '#3F93D2', '#44AA99', 
+                '#FE6100', '#DC267F', '#FFB000')
 names(cbbPalette) <- full_names
+
 
 getNemenyiCD <- function (alpha = 0.05, 
                           num.alg, 
                           num.problems) {
-  #' Auxiliar function to compute the critical difference for Nemenyi test
-  #' @Parms:
-  #'   alpha:        Alpha for the test
-  #'   num.alg:      Number of algorithms tested
-  #'   num.problems: Number of problems where the algorithms have been tested
-  #'
-  #' @Returns:
-  #'   Corresponding critical difference
-  
+    
+    #' @title Nemenyi Critical Difference values
+    #'
+    #' @description: Auxiliar function to compute the critical difference values after Nemenyi test
+    #'
+    #' @parm alpha num,        Alpha value for the test
+    #' @parm num.alg num,      Number of algorithms tested
+    #' @parm num.problems num, Number of problems where the algorithms have been tested
+    #'
+    #' @returns cd num,        Corresponding critical difference
+    #'
+    #' Reference:
+    #'   - Demsar, J. Statistical Comparisons of Classifiers over Multiple Data Sets. Journal of
+    #'   Machine Learning Research 2006, 7, 1–30.
+
   df <- num.alg * (num.problems - 1)
-  qa <- qtukey(p=1 - alpha, nmeans=num.alg, df=df)/sqrt(2)
+  qa <- qtukey(p = 1 - alpha, nmeans = num.alg, df = df) / sqrt(2)
   cd <- qa * sqrt((num.alg * (num.alg + 1)) / (6 * num.problems))
   return(cd)
 }
 
-plot_Critical_Difference_color <- function (results.matrix, alpha=0.05, cex=0.75, colPalette=cbbPalette, 
-            side_marging=0, line.spacing=0.9, labels.cex=0.6, ...) {
+
+plot_Critical_Difference_color <- function (
+        results.matrix, 
+        alpha = 0.05, 
+        cex   = 0.75, 
+        colPalette   = cbbPalette, 
+        side_marging = 0, 
+        line.spacing = 0.9, 
+        labels.cex   = 0.6, 
+        ...) {
   
+    #' @title Critical Difference Plot
+    #'
+    #' @description Auxiliar function to plot the critical difference plots. Taken from `github.com/cran/scmamp`
+    #'
+    #' @param matrix results.matrix,    Matrix with the performance results, 
+    #'                                        with N evaluations (rows) and k methods (cols)  
+    #' @param num alpha,                Alpha value for the test
+    #' @param num cex,                  `cex` value to render `CD value` text
+    #' @param vector colPalette,        Color palette for the k methods (cols)
+    #' @param num side_marging,         Marging value for the plot's width
+    #' @param num line.spacing,         Line spacing to render methods' labels
+    #' @param num labels.cex,           `cex` value to render methods' labels
+    #'
+    #' Reference:
+    #'   - Nemenyi, P. B. Distribution-Free Multiple Comparisons. Ph.D. thesis, Princeton University, 1963. 
+    #'
+    #' Modified from: 
+    #'  https://github.com/cran/scmamp/blob/master/R/plotting.R
+
+    
   opar <- par(mai = c(0,0,0,0))
   on.exit(par(opar))
   
-  k <- dim(results.matrix)[2]
-  N <- dim(results.matrix)[1]
+  k  <- dim(results.matrix)[2]
+  N  <- dim(results.matrix)[1]
   cd <- getNemenyiCD(alpha=alpha, num.alg=k, num.problems=N)
     
-  ## Get color palette
-#   colPalette <- hue_pal()(k)
-#   names(cbbPalette) <- rev(colnames(results.matrix))
   
   mean.rank <- sort(colMeans(rankMatrix(results.matrix, ...)))
   
   # Separate the algorithms in left and right parts
-  lp <- floor(k/2)
-  left.algs <- mean.rank[1:lp]
-  left.colors <- colPalette[names(left.algs)]
-  right.algs <- mean.rank[(lp+1):k]
+  lp           <- floor(k/2)
+  left.algs    <- mean.rank[1:lp]
+  left.colors  <- colPalette[names(left.algs)]
+  right.algs   <- mean.rank[(lp+1):k]
   right.colors <- colPalette[names(right.algs)]
-  max.rows <- ceiling(k/2)
+  max.rows     <- ceiling(k/2)
   
   # Basic dimensions and definitions
-  char.size    <- 0.001  # Character size
-  line.spacing <- line.spacing   # Line spacing for the algorithm name
+  char.size    <- 0.001            # Character size
+  line.spacing <- line.spacing     # Line spacing for the algorithm name
   m            <- floor(min(mean.rank))
   M            <- ceiling(max(mean.rank))
   max.char     <- max(sapply(colnames(results.matrix), FUN = nchar))  # Longest length of a label
   text.width   <- (max.char + 8) * char.size
   w            <- (M-m) + 2 * text.width + side_marging
-  h.up         <- 2.5 * line.spacing  # The upper part is fixed. Extra space is for the CD
+  h.up         <- 2.5 * line.spacing               # The upper part is fixed. Extra space is for the CD
   h.down       <- (max.rows + 2.25) * line.spacing # The lower part depends on the no. of algorithms. 
-  # The 2 extra spaces are for the lines that join algorithms
+                                                   # The 2 extra spaces are for the lines that join algorithms
   tick.h       <- 0.25 * line.spacing 
   
-  label.displacement <- 1    # Displacement of the label with respect to the axis
+  label.displacement <- 1      # Displacement of the label with respect to the axis
   line.displacement  <- 0.025  # Displacement for the lines that join algorithms
   
   # Background of the plot
-  plot(0, 0, type="n", xlim=c(m - w / (M - m), M + w / (M - m)), 
-       ylim=c(-h.down, h.up), xaxt="n", yaxt="n", xlab= "", ylab="", bty="n")
+  plot(0, 0, type = "n", 
+       xlim = c(m - w / (M - m), M + w / (M - m)), 
+       ylim = c(-h.down, h.up), 
+       xaxt = "n", yaxt = "n", 
+       xlab = "", ylab = "", 
+       bty  = "n")
   
   # Draw the axis
   lines (c(m,M), c(0,0), lwd=2)
   dk <- sapply(m:M, 
-               FUN=function(x) {
-                 lines(c(x,x), c(0, tick.h))
-                 text(x, 3*tick.h, labels=x, cex=cex)
+               FUN = function(x) {
+                 lines(c(x,x), 
+                       c(0, tick.h)
+                      )
+                 text(x, 3*tick.h, 
+                      labels = x, 
+                      cex = cex)
                })
   
   # Draw the critical difference
@@ -85,7 +129,7 @@ plot_Critical_Difference_color <- function (results.matrix, alpha=0.05, cex=0.75
   
   # Left part, labels
   dk <- sapply (1:length(left.algs), 
-                FUN=function(x) {
+                FUN = function(x) {
                   line.h <- -line.spacing * (x + 2)
                   text(x=m - label.displacement, y=line.h, 
                        labels=names(left.algs)[x], cex=labels.cex, adj=1)
@@ -97,7 +141,7 @@ plot_Critical_Difference_color <- function (results.matrix, alpha=0.05, cex=0.75
   
   # Right part, labels
   dk <- sapply (1:length(right.algs), 
-                FUN=function(x) {
+                FUN = function(x) {
                   line.h <- -line.spacing * (x + 2)
                   text(x=M + label.displacement, y=line.h, 
                        labels=names(right.algs)[x], cex=labels.cex, adj=0)
@@ -109,16 +153,16 @@ plot_Critical_Difference_color <- function (results.matrix, alpha=0.05, cex=0.75
   
   # Draw the lines to join algorithms
   getInterval <- function (x) {
-    from <- mean.rank[x]
-    diff <- mean.rank - from
-    ls <- which(diff > 0 & diff < cd)
-    if (length(ls) > 0) {
-      c(from, mean.rank[max(ls)])
-    }
+                    from <- mean.rank[x]
+                    diff <- mean.rank - from
+                    ls   <- which(diff > 0 & diff < cd)
+                    if (length(ls) > 0) {
+                      c(from, mean.rank[max(ls)])
+                    }
   }
   
   intervals <- mapply (1:k, FUN=getInterval)
-  aux <- do.call(rbind, intervals)
+  aux       <- do.call(rbind, intervals)
   if(NROW(aux) > 0) {
     # With this strategy, there can be intervals included into bigger ones
     # We remove them in a sequential way
